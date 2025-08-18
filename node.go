6bb48d3da894ba95483431d17c62673189184604
Khalid Mosaad <kmosaad26@gmail.com>
2025-08-18 12:17:34 +0300

    add pairs and implement root node conversion to page

--- node.go
+++ node.go
@@ -1,5 +1,7 @@
 package sapling
 
+import "errors"
+
 type NodeType uint8
 
 const (
@@ -8,18 +10,50 @@ const (
 	LEAF_NODE
 )
 
+type pair struct {
+	key   string
+	value string
+}
+
 // Node is the in-memory representation of the in-disk page
 type node struct {
-	ID       uint32
-	children []*node
-	parent   *node
-	typ      NodeType
+	ID uint32
+	// last child in this array is the right-most reference only has a value (reference) only applied for non-leaf nodes
+	children  []*node
+	parent    *node
+	typ       NodeType
+	dirty     bool
+	pairs     []pair
+	rightMost *uint32
 }
 
 // Get the disk page from the current node reference
-func (n *node) page() (*page, error) {
+func (n *node) page(db *BTree) (*page, error) {
 	// TODO: convert the node into page
-	return nil, nil
+	page := &page{
+		header: header{
+			pageID: n.ID,
+		},
+	}
+
+	if n.typ == ROOT_NODE {
+		page.header.freeStart = 16 // the header size
+		page.header.freeEnd = uint16(db.pageSize)
+		page.header.typ = ROOT_PAGE
+		page.header.cellCount = 0
+		page.pointers = []pointer{}
+		page.cells = []cell{}
+	} else if n.typ == INTERNAL_NODE {
+		return nil, errors.New("INTERNAL_NODE Not implemented yet...")
+	} else if n.typ == LEAF_NODE {
+		return nil, errors.New("LEAF_NODE Not implemented yet...")
+	} else {
+		return nil, errors.New("unknown node type")
+	}
+
+	// assume that the children always sorted
+
+	return page, nil
 }
 
 // Add new child to the current node
@@ -39,5 +73,17 @@ func (n *node) delete() error {
 // Update the current node
 func (n *node) update(value any) error {
 	// TODO: Update the current node with the sent value
+	return nil
+}
+
+// Split the current node
+func (n *node) split() error {
+	// TODO: Split the current node into two sibling nodes
+	return nil
+}
+
+// Merge the current node
+func (n *node) merge(sibling *node) error {
+	// TODO: Merge the current node with it's sibling
 	return nil
 }
