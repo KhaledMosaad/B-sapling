013a34eb397679d7c64f9e78cbb1c83209dbfcfe
Khalid Mosaad <kmosaad26@gmail.com>
2025-08-18 12:18:29 +0300

    add testing for adding root node

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+
+	sapling "github.com/KhaledMosaad/B-sapling"
 )
 
 func main() {
 	fmt.Println(os.Getpagesize())
+
+	_, err := sapling.Open("./db/fast.db")
+
+	if err != nil {
+		panic(err)
+	}
+
 }
